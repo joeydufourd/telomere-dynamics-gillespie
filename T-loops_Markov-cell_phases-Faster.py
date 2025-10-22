5219937d71b28088647e0647ce881474daa91e66
Joey Dufourd <joey.dufourd@gmail.com>
2025-10-22 22:28:34 +0200

    Numba improved simulations, ~10 times faster

--- T-loops_Markov-cell_phases-Faster.py
+++ T-loops_Markov-cell_phases-Faster.py
@@ -2,11 +2,16 @@ import numpy
 import math
 import matplotlib.pyplot as plt
 import time
+from numba import njit
+from numba.typed import List
+from numba import prange
 
 # Initialize species counts: [LB, LR, T] ; LB is Linear Blunt telomeres, LR Linear resected Telomeres, T Telomere-loops
-X = [46, 46, 0]
-#start = time.time()
+X = numpy.array([46, 46, 0], dtype=numpy.float64)
+
 # Parameters
+start = time.time()
+numpy.random.seed(0)
 numspecies = 3 #LB/LR/T
 numreactions = 5 #Detailed in smatrix
 numsim = 5 #number of simulations (=number of cells simulated)
@@ -17,25 +22,27 @@ G2_len = 2*3600 #G2 is approx 2 hours in ESCs
 cycle_length = G1_len + S_len + G2_len
 
 # Stoichiometry matrix, refer to the diagram for a more visual representation
-smatrix = [
-    [-1, 1, 0],  # LB->LR (APOLLO)
-    [1, -1, 0],  # LR->LB (Breaks, DNAses)
-    [0, -1, 1],  # LR->T (Telomere loop formation)
-    [1, 0, -1],  # T->LB (T-circle, breaks)
-    [0, 1, -1]   # T->LR (Opening of the t-loop, C-circle)
-]
+smatrix = numpy.array([
+    [-1, 1, 0],
+    [1, -1, 0],
+    [0, -1, 1],
+    [1, 0, -1],
+    [0, 1, -1]
+])
 
 # Rate constants for each phase
-k_G1  = [0.1, 0.00001, 1.0, 0.00001, 4.0]  # G1 less LB/LR more t-loops
-k_S   = [1, 1, 1.0, 0.00001, 2.0] # S phase less t-loops to replicate, equilibrium for LB/LR [1.0, 1.0, 0.1, 0.00001, 2.0]
-k_G2M = [0.1, 0.00001, 1.0, 0.00001, 4.0]  # G2/M back to G1 like
+k_G1  = numpy.array([0.1, 0.00001, 1.0, 0.00001, 4.0])  # G1 less LB/LR more t-loops
+k_S   = numpy.array([1, 1, 1.0, 0.00001, 2.0]) # S phase less t-loops to replicate, equilibrium for LB/LR [1.0, 1.0, 0.1, 0.00001, 2.0]
+k_G2M = numpy.array([0.1, 0.00001, 1.0, 0.00001, 4.0])  # G2/M back to G1 like
 
 # Gillespie propensity calculations
+@njit
 def propensity(X, k):
     R = numpy.array([k[0]*X[0], k[1]*X[1], k[2]*X[1], k[3]*X[2], k[4]*X[2]])
     return R, R.sum()
 
 # Gillespie step (choosing which reaction to do at time t)
+@njit
 def Next(X, t, k):
     R, RTot = propensity(X, k)
     if RTot == 0:
@@ -43,25 +50,28 @@ def Next(X, t, k):
     #Gillespie method, formula and time increment
     V = numpy.random.random() * RTot
     i = numpy.searchsorted(numpy.cumsum(R), V)
-    X = X + numpy.array(smatrix[i])
+    X = X + smatrix[i]
     tau = -numpy.log(numpy.random.random()) / RTot
     return t + tau, X
 
 # Division functions, most divisions result in equal partition of DNA material
+@njit
 def deterministic_partition(X):
-    X = numpy.asarray(X, dtype=int)
+    X = numpy.asarray(X, dtype=numpy.float64)
     Xd = X // 2
     X_rem = X - Xd
     return Xd + X_rem
 
 # Rare non equal division
+@njit
 def partition_with_rare_noise(X, eps=1e-10):
     if numpy.random.random() < eps:
-        return numpy.array([numpy.random.binomial(int(x), 0.5) for x in X], dtype=int)
+        return numpy.array([numpy.random.binomial(int(x), 0.5) for x in X], dtype=numpy.float64)
     else:
         return deterministic_partition(X)
 
 # Simulation using time-based scaling
+@njit
 def simulate_cycle(X0, tmax):
     t_total = 0
     X = X0.copy()
@@ -93,19 +103,29 @@ def simulate_cycle(X0, tmax):
         if t_in_cycle + tau >= cycle_length:
             X = partition_with_rare_noise(X)
 
-    return numpy.array(Xz), numpy.array(Yz), X
+    return Xz, Yz, X
 
-# Main program
-def programme(X, numsim):
-    all_Xz = []
-    all_Yz = []
+@njit(parallel=True)
+def parallel_sim(X, numsim):
+    all_Xz = List()
+    all_Yz = List()
 
-    for sim in range(numsim):
-        print(f"Currently at simulation #{sim+1}")
+    for sim in prange(numsim):
+        #print(f"Currently at simulation #{sim+1}")
         Xz, Yz, Xfinal = simulate_cycle(X, tmax)
+        #Xz=numpy.array(Xz)
+        #Yz = numpy.array(Yz)
         all_Xz.append(Xz)
         all_Yz.append(Yz)
+    return all_Xz, all_Yz
 
+
+# Main program
+
+def programme(X, numsim):
+    all_Xz, all_Yz = parallel_sim(X,numsim)
+    all_Yz = [numpy.array(y) for y in all_Yz]
+    all_Xz = [numpy.array(x) for x in all_Xz]
     # Plot
     species_names = ["Blunt linear", "Resected linear", "T-loop"]
     for species_idx, species_name in enumerate(species_names):
@@ -116,7 +136,7 @@ def programme(X, numsim):
         # Interpolation for better data visualisation
         Y_interp = []
         for sim in range(numsim):
-            Y_sim = numpy.array(all_Yz[sim][:, species_idx])
+            Y_sim = numpy.array(all_Yz[sim])[:, species_idx]
             t_sim = numpy.array(all_Xz[sim])
             Y_interp.append(numpy.interp(t_grid, t_sim, Y_sim))
         Y_interp = numpy.array(Y_interp)
@@ -147,9 +167,9 @@ def programme(X, numsim):
         plt.ylim(0, 100)
         plt.xlim(0,tmax)
         plt.legend()
-        plt.show()
-    #end = time.time() #5 sim  Execution time: 878.0635 seconds
-    #print(f"Execution time: {end - start:.4f} seconds")
+        #plt.show()
+    end = time.time() # njit improved Execution time: 102.9640 seconds, 8 times improvement (5sim) ; parallel improves to Execution time: 70.7025 seconds (5sim)
+    print(f"Execution time: {end - start:.4f} seconds")
 
-    # Run !
+# Run !
 programme(X, numsim)
